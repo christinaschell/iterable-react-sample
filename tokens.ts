4fdbd26340e79408570ff0382a6c5c35650fe077
christinaschell <86267480+christinaschell@users.noreply.github.com>
2021-10-26 09:28:06 -0700

    Delete tokens.ts

--- tokens.ts
+++
@@ -1,9 +0,0 @@
-export class Tokens {
-    public apiKey: string;
-    email: string;
-    
-    constructor() {
-        this.apiKey = "349dcc9373c74c6699c5d1204a271695"
-        this.email = "docs@iterable.com"
-    }
-}
